e6bd12b88bc3c9e6bb8d84e63419e5dec784e194
hej <hej@Maltes-Mac-mini.local>
2024-10-02 18:49:36 +0200

    add zip step

--- scripts/mac_build.sh
+++ scripts/mac_build.sh
@@ -6,3 +6,4 @@
 make clean; make && cp -R bin/* ../Glyphica
 cd ../Glyphica
 dylibbundler -od -b -x ./neosphere -d ./libs -p @executable_path/libs
+zip -r glyphica_intel_mac.zip neosphere libs libsteam_api.dylib
